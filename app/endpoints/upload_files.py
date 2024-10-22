903dd681e145a703dc4b38a6d089a292e25e53f0
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-10-22 17:12:36 -0600

    fix: process for read file pass from read 1st row has head to read each line with key and value separated by commas

--- app/endpoints/upload_files.py
+++ app/endpoints/upload_files.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends, UploadFile
+from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
 
 from app.config.logging import get_logger
+from app.crud import crud_file
 from app.database.db import get_db
 from app.schemas import CsvModelBase
+from app.utils.constants import fields
 from app.utils.custom_datetime_type import CustomDateTimeType
-from app.crud import crud_file
 
 router = APIRouter()
 logger = get_logger()
@@ -18,40 +20,47 @@ async def process_file(file: UploadFile, db: Session = Depends(get_db)):
 
     schema_list = []
     current_record = {}
-    for line in file_content:
-        if not line.strip():
-            continue
+    try:
+        for line in file_content:
+            if not line.strip():
+                continue
 
-        key, value = line.split(",", 1)
+            key, value = line.split(",", 1)
 
-        value = value.strip().strip('"')
+            value = value.strip().strip('"')
 
-        if key in current_record:
-            if len(current_record) == 5:
-                schema_list.append(CsvModelBase(
-                    record_id=current_record.get("ID"),
-                    name=current_record.get("Nombre"),
-                    bird_date=CustomDateTimeType().convert_str_to_datetime(current_record.get("Fecha")),
-                    email=current_record.get("Email"),
-                    amount=float(current_record.get("Monto"))
-                ))
-                current_record = {}
+            if key not in fields:
+                return HTTPException(status_code=400,
+                                        detail=f"Error processing file: field {key} not allowed.")
+            if key in current_record:
+                if len(current_record) == 5:
+                    schema_list.append(CsvModelBase(
+                        record_id=current_record.get("ID"),
+                        name=current_record.get("Nombre"),
+                        bird_date=CustomDateTimeType().convert_str_to_datetime(current_record.get("Fecha Nacimiento")),
+                        email=current_record.get("Email"),
+                        amount=float(current_record.get("Monto"))
+                    ))
+                    current_record = {}
 
-        current_record[key] = value
+            current_record[key] = value
 
-    if current_record:
-        schema_list.append(CsvModelBase(
-            record_id=current_record.get("ID"),
-            name=current_record.get("Nombre"),
-            bird_date=CustomDateTimeType().convert_str_to_datetime(current_record.get("Fecha")),
-            email=current_record.get("Email"),
-            amount=float(current_record.get("Monto"))
-        ))
+        if current_record:
+            schema_list.append(CsvModelBase(
+                record_id=current_record.get("ID"),
+                name=current_record.get("Nombre"),
+                bird_date=CustomDateTimeType().convert_str_to_datetime(current_record.get("Fecha Nacimiento")),
+                email=current_record.get("Email"),
+                amount=float(current_record.get("Monto"))
+            ))
+    except Exception as exc:
+        return HTTPException(status_code=400,
+                                 detail=f"Error processing file {exc}")
 
     try:
         return crud_file.create_bulk(db=db, obj_in=schema_list)
     except Exception as exc:
-        logger.warning(f"Error intentando serializar datos del archivo: {exc}")
+        logger.warning(f"Error intentando insertar datos en base de datos: {exc}")
 
 
 
