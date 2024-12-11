5e97d76ab973f4e8b2d914437e80b47b5b538574
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-12-10 21:07:51 -0700

    process xml with xml elementee

--- process_xml.py
+++ process_xml.py
@@ -1,20 +1,35 @@
-import xml.etree.ElementTree as et
+import re
+import xml.etree.ElementTree as ET
 
-file = et.parse("data.xml")
+xml_tree = ET.parse("data.xml")
+root = xml_tree.getroot()  # Obtén el nodo raíz del XML (el nodo <people>)
 
-xml_path = [
-    (
-        "cfdi", {
-        "TiposFiguraDomicilio": ("cfdi:Comprobante", "cfdi:Complemento" "cartaporte31:Cartaporte", "cartaporte31:FiguraTransporte", "cartaporte31:TiposFigura", "cartaporte31:Domicilio", "@Calle"),
-        "UbicacionDomicilio": ("cfdi:Comprobante", "cfdi:Complemento" "cartaporte31:Cartaporte", "cartaporte31:Ubicaciones", "cartaporte31:Ubicacion", "cartaporte31:Domicilio", "@Pais")
-    },
-    )
-]
-for a, b in xml_path:
-    # print(a)
-    print(b)
-
-data_to_dict = {
-    "TiposFiguraDomicilio": [],
-    "UbicacionDomcilio": []
+namespaces = {
+    "cfdi": "http://www.sat.gob.mx/cfd/4"
 }
+
+dic = {
+    "concepto_impuesto": ".//cfdi:conceptos//cfdi:traslados//cfdi:traslado[@Impuesto]",
+    "impuesto_impuesto": ".//cfdi:impuestos//cfdi:traslados//cfdi:traslado[@Impuesto]"
+}
+
+for a, b in dic.items():
+    print("aasdasddas", a,  b)
+
+# Recorrer los paths definidos en el diccionario
+for key, value in dic.items():
+    traslado = root.find(value, namespaces)
+    print(traslado)
+
+    attrib_pattern = r"\[@([^\]]+)\]"
+    attrib_name = re.search(attrib_pattern, value).group(1)
+    if traslado is not None:
+        print("++++++++++++++++++++++++++=")
+        print(traslado.attrib[attrib_name])
+        traslado = 0
+    print("attrib_name ", attrib_name)
+    print(f"KEY => {key} VALUE => {value} RESULTADO => {traslado.attrib.get(attrib_name, 0)}")
+
+    if traslado is not None:
+        impuesto_value = traslado.attrib.get(attrib_name, "No encontrado")
+        print(f"  Impuesto: {impuesto_value}")
